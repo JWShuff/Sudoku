ca1bbe203d3ce5b177ec7182303b2b34df14a600
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-12 12:02:54 -0400

    Working board print out, and conversion of input string to an array for further processing.

--- sudoku.py
+++ sudoku.py
@@ -1,14 +1,49 @@
 class SudokuSolver:
     def __init__(self, board_string):
         self.board_string = board_string
+        self.board_array = self.string_to_grid()
+
+    # This logic converts the input 81 character string to a 2d array:
+    # where the first [] = the row (0-8), and the second [] indicates column.
+    def string_to_grid(self):
+        lst = []
+        for i, _ in enumerate(self.board_string):
+            if i % 9 == 0:
+                sub = self.board_string[i:i+9:1]
+                lst.append(sub)
+        return(lst)
+
+    def check_column(self, index):
+        if board_string[index] != 0:
+            return
+        
 
     def solve(self):
+        # To make the logic more clear,
+        # convert the input string to a list of
+        # 9 sub lists, so [row][column] becomes the addresses in question
+            
+        # Looks up and down the column while removing possible options for non 0 values at indices.
+
+        # Check row
+        # Check box
+
+        # Working with a string we need an answer key for index values
+        # in relative positioning:
+        # Row 1 i : 0-8     Column 1: i: 0, 9,18,27,36,45,54,63,72
+        # Row 2 i : 9-17    Column 2: i: 1,10,19,28,37,46,55,64,73
+        # Row 3 i : 18-26   Column 3: i: 2,11,20,29,38,47,56,65,74
+        # Row 4 i : 27:35   Column 4: i: 3,12,21,30,39,48,57,66,75
+        # Row 5 i : 36:44   Column 5: i: 4,13,22,31,40,49,58,67,76
+        # Row 6 i : 45:53   Column 6: i: 5,14,23,32,41,50,59,68,77
+        # Row 7 i : 54:62   Column 7: i: 6,15,24,33,42,51,60,69,78
+        # Row 8 i : 63:71   Column 8: i: 7,16,25,34,43,52,61,70,79
+        # Row 9 i : 72:80   Column 9: i: 8,17,26,35,44,53,62,71,80
         pass
 
-
     def board(self):
-        formatted_board = "----------------------\n"
         box_format_string = "----------------------\n"
+        formatted_board = box_format_string
         # Prints the board string in the appropriate format:
         for i, value in enumerate(self.board_string):
             if i % 9 == 0:
@@ -40,7 +75,7 @@ class SudokuSolver:
   
 game = SudokuSolver("619030040270061008000047621486302079000014580031009060005720806320106057160400030")
 print(game.board())
-
+print(game.board_array)
 # The file has newlines at the end of each line, so we call
 # String#chomp to remove them.
 # game = SudokuSolver(board_string)
